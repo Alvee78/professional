b630328931d49806915ec5e86c7454a5de78c4c1
Alvee <alvee.rucse@gmail.com>
2024-10-04 21:24:06 +0600

    STEP #10.	Add more comments and makes more naming improvements.

--- StudentList.java
+++ StudentList.java
@@ -5,46 +5,49 @@ import java.util.*;
 public class StudentList {
     public static void main(String[] args) {
 
-//		Check arguments
+        //	Check arguments
         if (args == null || args.length != 1 ) {
             System.out.println(Constants.INPUT_ARG);
-            return;
+            return; //Early exit
         }
-
-        String fileContents = readFileContents(Constants.FILE_NAME);
+        // Every operation need to load data
+        String studentList = loadStudentList(Constants.FILE_NAME); // Contains Student list
         System.out.println(Constants.LOADING_DATA_TEXT);
 
         if (args[0].equals(Constants.SHOW_ALL)) {
-            String words[] = fileContents.split(Constants.STUDENT_ENTRY_DELIMITER);
-            for (String word : words) {
-                System.out.println(word);
+            String students[] = studentList.split(Constants.STUDENT_ENTRY_DELIMITER);
+            for (String student : students) {
+                System.out.println(student);
             }
         } else if (args[0].equals(Constants.SHOW_RANDOM)) {
-            String words[] = fileContents.split(Constants.STUDENT_ENTRY_DELIMITER);
-            int randomNumber = new Random().nextInt(words.length);
-            System.out.println(words[randomNumber]);
+            String students[] = studentList.split(Constants.STUDENT_ENTRY_DELIMITER);
+            int randomNumber = new Random().nextInt(students.length);
+            System.out.println(students[randomNumber]);
+
         } else if (args[0].contains(Constants.ADD_ENTRY)) {
-            String word = args[0].substring(1);
+            String studentEntry = args[0].substring(1);
             String formatedDate = new SimpleDateFormat(Constants.DATE_FORMAT).format(new Date());
-            writeToFile(Constants.FILE_NAME, ", " + word + "\nList last updated on " + formatedDate);
+            updateStudentList(Constants.FILE_NAME, ", " + studentEntry + "\nList last updated on " + formatedDate);
+
         } else if (args[0].contains(Constants.FIND_ENTRY)) {
-            String words[] = fileContents.split(Constants.STUDENT_ENTRY_DELIMITER);
-            String word = args[0].substring(1);
+            String students[] = studentList.split(Constants.STUDENT_ENTRY_DELIMITER);
+            String searchTerm = args[0].substring(1);
             int indexLocation = -1;
-            for (int idx = 0; idx < words.length; idx++) {
-                if ((words[idx].trim()).equals(word.trim())) {
+            for (int idx = 0; idx < students.length; idx++) {
+                if ((students[idx].trim()).equals(searchTerm.trim())) {
                     indexLocation = idx;
                     break;
                 }
             }
+            //
             if(indexLocation >= 0) {
-                System.out.println("Entry " + word + " found at location : " + indexLocation);
+                System.out.println("Entry " + searchTerm + " found at location : " + indexLocation);
             } else {
-                System.out.println("Entry " + word + " does not exists");
+                System.out.println("Entry " + searchTerm + " does not exists");
             }
 
         } else if (args[0].contains(Constants.SHOW_COUNT)) {
-            String[] words = fileContents.split(Constants.STUDENT_ENTRY_DELIMITER);
+            String[] words = studentList.split(Constants.STUDENT_ENTRY_DELIMITER);
             System.out.println(words.length + Constants.WORDS_FOUND_TEXT);
         } else {
             System.out.println("Invalid argument: " + args[0]);
@@ -53,8 +56,8 @@ public class StudentList {
 
         System.out.println(Constants.DATA_LOADED_TEXT);
     }
-
-    public static String readFileContents(String fileName) {
+    //To load student list
+    public static String loadStudentList(String fileName) {
         try {
             BufferedReader bufferedReader = new BufferedReader(
                                             new InputStreamReader(
@@ -65,7 +68,8 @@ public class StudentList {
         }
     }
 
-    public static void writeToFile(String fileName, String data) {
+    // To entry new student data
+    public static void updateStudentList(String fileName, String data) {
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(
                                             new FileWriter(fileName, true));
